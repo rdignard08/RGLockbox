a0c505dc3a2b6ff0b3d8bdd14809dfa6e680e1d8
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-01-26 23:39:34 -0800

    replacement primitive implementation

--- RGLockbox/RGLockbox.h
+++ RGLockbox/RGLockbox.h
@@ -1,14 +1,27 @@
+
+#import <Foundation/Foundation.h>
+
 @interface RGLockbox : NSObject
 
 /**
- Returns the singleton instance for managing access to the key chain.
+ Returns the singleton instance for managing access to the key chain.  Uses the default namespace.
  */
 + (instancetype) manager;
 
 /**
+ Returns an instance of `RGLockbox` which is not default namespaced.
+ */
+- (instancetype) initWithNamespace:(NSString*)namespace accessibility:(NSString*)accessibility NS_DESIGNATED_INITIALIZER;
+
+/**
  Defaults to the app bundle identifier if unspecified.
  */
-@property (nonatomic, strong) NSString* nameSpace;
+@property (nonatomic, strong, readonly) NSString* namespace;
+
+/**
+ The default accessibility when checking the keychain, defaults to `kSecAttrAccessibleAfterFirstUnlock`.
+ */
+@property (nonatomic, strong, readonly) NSString* itemAccessibility;
 
 /**
  Return the object set on key in the current namespace.  Only supports NSData at the moment.
@@ -16,81 +29,71 @@
 - (NSData*) objectForKey:(NSString*)key;
 
 /**
- Allows `[RGLockbox manager][key]` syntax for the above.
- */
-- (id /* NSData */) objectForKeyedSubscript:(id<NSCopying, NSObject>)key;
-
-/**
- Sets the object provided on the key in the current namespace
- */
-- (void) setObject:(NSData*)object forKey:(id<NSCopying>)key;
-
-/**
  Allows `[RGLockbox manager][key] = object` syntax for the above.
  */
-- (void) setObject:(id /* NSData */)object forKeyedSubscript:(id<NSCopying, NSObject>)key;
+- (void) setObject:(NSData*)object forKey:(NSString*)key;
 
 
-// vvvv Old interface vvvv
-
-/**
- @return `YES` in the event of success; otherwise `NO`.  Error messages are hard :(
- 
- This is the "raw" insertion method.
- */
-+ (BOOL) setData:(NSData*)value forKey:(NSString*)key;
-
-+ (BOOL) setData:(NSData*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
-
-/**
- @return the last value that was set for the given key.
- 
- This is the "raw" access method.
- */
-+ (NSData*) dataForKey:(NSString*)key;
-
-+ (NSData*) dataForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
-
-/**
- @throw NSInvalidArgument Will bail if `object` cannot be serialized by `NSJSONSerialization`.
- */
-+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key;
-
-+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
-
-/**
- If saved with +setJSONObject:forKey: the value can be recovered from this method.
- */
-+ (id) objectForKey:(NSString*)key;
-
-+ (id) objectForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
-
-/**
- Dates are not part of the JSON standard so they need to be handled in a separate way.
- */
-+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key;
-
-+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
-
-/**
- Retrieve a date set with +setDate:forKey:
- */
-+ (NSDate*) dateForKey:(NSString*)key;
-
-+ (NSDate*) dateForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
-
-/**
- Strings aren't allowed as top-level objects in JSON, and most things can become strings easily; hence this method.
- */
-+ (BOOL) setString:(NSString*)value forKey:(NSString*)key;
-
-+ (BOOL) setString:(NSString*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
-
-/**
- Returns as a string, the value set for key
- */
-+ (NSString*) stringForKey:(NSString*)key;
-
-+ (NSString*) stringForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//// vvvv Old interface vvvv
+//
+///**
+// @return `YES` in the event of success; otherwise `NO`.  Error messages are hard :(
+// 
+// This is the "raw" insertion method.
+// */
+//+ (BOOL) setData:(NSData*)value forKey:(NSString*)key;
+//
+//+ (BOOL) setData:(NSData*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//
+///**
+// @return the last value that was set for the given key.
+// 
+// This is the "raw" access method.
+// */
+//+ (NSData*) dataForKey:(NSString*)key;
+//
+//+ (NSData*) dataForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//
+///**
+// @throw NSInvalidArgument Will bail if `object` cannot be serialized by `NSJSONSerialization`.
+// */
+//+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key;
+//
+//+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//
+///**
+// If saved with +setJSONObject:forKey: the value can be recovered from this method.
+// */
+//+ (id) objectForKey:(NSString*)key;
+//
+//+ (id) objectForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//
+///**
+// Dates are not part of the JSON standard so they need to be handled in a separate way.
+// */
+//+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key;
+//
+//+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//
+///**
+// Retrieve a date set with +setDate:forKey:
+// */
+//+ (NSDate*) dateForKey:(NSString*)key;
+//
+//+ (NSDate*) dateForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//
+///**
+// Strings aren't allowed as top-level objects in JSON, and most things can become strings easily; hence this method.
+// */
+//+ (BOOL) setString:(NSString*)value forKey:(NSString*)key;
+//
+//+ (BOOL) setString:(NSString*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
+//
+///**
+// Returns as a string, the value set for key
+// */
+//+ (NSString*) stringForKey:(NSString*)key;
+//
+//+ (NSString*) stringForKey:(NSString*)key inNameSpace:(NSString*)nameSpace;
 
 @end
--- RGLockbox/RGLockbox.m
+++ RGLockbox/RGLockbox.m
@@ -3,8 +3,56 @@
 
 static NSString* const _sIsoFormat = @"yyyy-MM-dd'T'HH:mm:ssZZZZZ";
 
+static NSString* rg_bundle_identifier(void) {
+    static NSString* _sBundleIdentifier;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sBundleIdentifier = [NSBundle bundleForClass:[RGLockbox self]].infoDictionary[(id)kCFBundleIdentifierKey];
+    });
+    return _sBundleIdentifier;
+}
+
+static NSData* rg_get_data_for_key(CFStringRef key) {
+    CFTypeRef data = nil;
+    CFTypeRef keys[] = { kSecClass, kSecAttrService, kSecReturnData };
+    CFTypeRef values[] = { kSecClassGenericPassword, key, kCFBooleanTrue };
+    CFDictionaryRef query = CFDictionaryCreate(nil, keys, values, sizeof(keys), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    SecItemCopyMatching(query, &data);
+    CFRelease(query);
+    return (__bridge_transfer NSData*)data;
+}
+
+static void rg_delete_data_for_key(CFStringRef key) {
+    CFTypeRef keys[] = { kSecClass, kSecAttrService };
+    CFTypeRef values[] = { kSecClassGenericPassword, key };
+    CFDictionaryRef query = CFDictionaryCreate(nil, keys, values, sizeof(keys), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    SecItemDelete(query);
+    CFRelease(query);
+}
+
+static void rg_update_data_for_key(CFDataRef data, CFMutableDictionaryRef itemQuery, CFStringRef accessibility) {
+    NSMutableDictionary* update = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
+    CFDictionarySetValue((__bridge CFMutableDictionaryRef)update, kSecValueData, CFDictionaryGetValue(itemQuery, kSecValueData));
+    CFDictionarySetValue((__bridge CFMutableDictionaryRef)update, kSecAttrAccessible, accessibility);
+    CFDictionaryRemoveValue(itemQuery, kSecAttrAccessible);
+    SecItemUpdate(itemQuery, (__bridge CFMutableDictionaryRef)update);
+}
+
+static void rg_set_data_for_key(CFDataRef data, CFStringRef key, CFStringRef accessibility) {
+    NSMutableDictionary* query = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
+    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecClass, kSecClassGenericPassword);
+    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrService, key);
+
+    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecValueData, data);
+    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrAccessible, accessibility);
+    
+    OSStatus status = SecItemAdd((__bridge CFMutableDictionaryRef)query, NULL);
+    if (status == errSecDuplicateItem) {
+        rg_update_data_for_key(data, (__bridge CFMutableDictionaryRef)query, accessibility);
+    }
+}
+
 @implementation RGLockbox
-@synthesize nameSpace = _nameSpace;
 
 + (instancetype) manager {
     static dispatch_once_t onceToken;
@@ -15,150 +63,144 @@ static NSString* const _sIsoFormat = @"yyyy-MM-dd'T'HH:mm:ssZZZZZ";
     return _sManager;
 }
 
-- (NSString*) nameSpace {
-    if (!_nameSpace) {
-        _nameSpace = [[self class] bundleIdentifier];
+- (instancetype) init {
+    return [self initWithNamespace:rg_bundle_identifier() accessibility:(__bridge NSString*)kSecAttrAccessibleAfterFirstUnlock];
+}
+
+- (instancetype) initWithNamespace:(NSString*)namespace accessibility:(NSString*)accessibility {
+    self = [super init];
+    if (self) {
+        self->_namespace = namespace;
+        self->_itemAccessibility = accessibility;
     }
-    return _nameSpace;
+    return self;
 }
 
-- (id) objectForKey:(NSString*)key {
-    return [[self class] dataForKey:key inNameSpace:self.nameSpace];
+- (NSData*) objectForKey:(NSString*)key {
+    NSString* hierarchyKey = self.namespace ? [NSString stringWithFormat:@"%@.%@", self.namespace, key] : key;
+    return rg_get_data_for_key((__bridge CFStringRef)hierarchyKey);
 }
 
-- (id) objectForKeyedSubscript:(id<NSCopying, NSObject>)key {
-    return [self objectForKey:key];
+- (void) setObject:(NSData*)object forKey:(NSString*)key {
+    NSString* hierarchyKey = self.namespace ? [NSString stringWithFormat:@"%@.%@", self.namespace, key] : key;
+    CFStringRef cfKey = (__bridge CFStringRef)hierarchyKey;
+    CFStringRef accessibility = (__bridge CFStringRef)self.itemAccessibility;
+    CFDataRef cfData = (__bridge CFDataRef)object;
+    object ? rg_set_data_for_key(cfData, cfKey, accessibility) : rg_delete_data_for_key(cfKey);
 }
 
-- (void) setObject:(id)object forKey:(id<NSCopying>)key {
-    [[self class] setData:value forKey:key inNameSpace:self.nameSpace];
-}
-
-- (void) setObject:(id)object forKeyedSubscript:(id<NSCopying, NSObject>)key {
-    [self setObject:object forKey:key];
-}
-
-+ (NSString*) bundleIdentifier {
-    static dispatch_once_t onceToken;
-    static NSString* _sBundleIdentifier;
-    dispatch_once(&onceToken, ^{
-        _sBundleIdentifier = [[NSBundle bundleForClass:[self class]] infoDictionary][(id)kCFBundleIdentifierKey];
-    });
-    return _sBundleIdentifier;
-}
-
-+ (BOOL) setData:(NSData*)value forKey:(NSString*)key {
-    return [self setData:value forKey:key inNameSpace:[self bundleIdentifier] accessibility:kSecAttrAccessibleAlways];
-}
-
-+ (BOOL) setData:(NSData*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    return [self setData:value forKey:key inNameSpace:nameSpace accessibility:kSecAttrAccessibleAlways];
-}
-
-+ (BOOL) setData:(NSData*)obj forKey:(NSString*)key inNameSpace:(NSString*)nameSpace accessibility:(CFTypeRef)accessibility {
-    NSString* hierarchyKey = [NSString stringWithFormat:@"%@.%@", nameSpace, key];
-    
-    NSMutableDictionary* query = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
-    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecClass, kSecClassGenericPassword);
-    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrService, (__bridge const void*)hierarchyKey);
-    
-    if (!obj) {
-        return SecItemDelete((__bridge CFMutableDictionaryRef)query) == errSecSuccess;
-    }
-
-    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecValueData, (__bridge const void*)obj);
-    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrAccessible, accessibility);
-
-    OSStatus status = SecItemAdd((__bridge CFMutableDictionaryRef)query, NULL);
-    if (status == errSecDuplicateItem) {
-        NSMutableDictionary* update = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
-        CFDictionarySetValue((__bridge CFMutableDictionaryRef)update, kSecValueData, CFDictionaryGetValue((__bridge CFMutableDictionaryRef)query, kSecValueData));
-        CFDictionarySetValue((__bridge CFMutableDictionaryRef)update, kSecAttrAccessible, accessibility);
-        CFDictionaryRemoveValue((__bridge CFMutableDictionaryRef)query, kSecAttrAccessible);
-        return (SecItemUpdate((__bridge CFMutableDictionaryRef)query, (__bridge CFMutableDictionaryRef)update) == errSecSuccess);
-    }
-    return status == errSecSuccess;
-}
-
-+ (NSData*) dataForKey:(NSString*)key {
-    return [self dataForKey:key inNameSpace:[self bundleIdentifier]];
-}
-
-+ (NSData*) dataForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    NSString* hierarchyKey = [NSString stringWithFormat:@"%@.%@", nameSpace, key];
-    NSMutableDictionary* query = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
-    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecClass, kSecClassGenericPassword);
-    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrService, (__bridge const void*)hierarchyKey);
-    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecReturnData, kCFBooleanTrue);
-    CFDataRef data;
-    if (SecItemCopyMatching((__bridge CFMutableDictionaryRef)query, (CFTypeRef*)&data) != errSecSuccess) {
-        return nil;
-    }
-    return (__bridge_transfer NSData*)data;
-}
-
-+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key {
-    return [self setJSONObject:object forKey:key inNameSpace:[self bundleIdentifier]];
-}
-
-+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    if (!object) {
-        return [self setData:nil forKey:key inNameSpace:nameSpace];
-    }
-    if (![NSJSONSerialization isValidJSONObject:object]) {
-        @throw NSInvalidArgumentException;
-    }
-    return [self setData:[NSJSONSerialization dataWithJSONObject:object options:0 error:nil] forKey:key inNameSpace:nameSpace];
-}
-
-+ (id) objectForKey:(NSString*)key {
-    return [self objectForKey:key inNameSpace:[self bundleIdentifier]];
-}
-
-+ (id) objectForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    NSData* data = [self dataForKey:key inNameSpace:nameSpace];
-    if (data) {
-        return [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-    }
-    return nil;
-}
-
-+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key {
-    return [self setDate:date forKey:key inNameSpace:[self bundleIdentifier]];
-}
-
-+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    NSDateFormatter* formatter = [NSDateFormatter new];
-    formatter.dateFormat = _sIsoFormat;
-    NSString* dateString = [formatter stringFromDate:date];
-    return [self setString:dateString forKey:key inNameSpace:nameSpace];
-}
-
-+ (NSDate*) dateForKey:(NSString*)key {
-    return [self dateForKey:key inNameSpace:[self bundleIdentifier]];
-}
-
-+ (NSDate*) dateForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    NSDateFormatter* formatter = [NSDateFormatter new];
-    formatter.dateFormat = _sIsoFormat;
-    return [formatter dateFromString:[self stringForKey:key inNameSpace:nameSpace]];
-}
-
-+ (BOOL) setString:(NSString*)value forKey:(NSString*)key {
-    return [self setString:value forKey:key inNameSpace:[self bundleIdentifier]];
-}
-
-+ (BOOL) setString:(NSString*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    return [self setData:[value dataUsingEncoding:NSUTF8StringEncoding] forKey:key inNameSpace:nameSpace];
-}
-
-+ (NSString*) stringForKey:(NSString*)key {
-    return [self stringForKey:key inNameSpace:[self bundleIdentifier]];
-}
-     
-+ (NSString*) stringForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
-    NSData* data = [self dataForKey:key inNameSpace:nameSpace];
-    return data ? [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] : nil;
-}
+//+ (BOOL) setData:(NSData*)value forKey:(NSString*)key {
+//    return [self setData:value forKey:key inNameSpace:[self bundleIdentifier] accessibility: kSecAttrAccessibleAlways];
+//}
+//
+//+ (BOOL) setData:(NSData*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    return [self setData:value forKey:key inNameSpace:nameSpace accessibility:kSecAttrAccessibleAlways];
+//}
+//
+//+ (BOOL) setData:(NSData*)obj forKey:(NSString*)key inNameSpace:(NSString*)nameSpace accessibility:(CFTypeRef)accessibility {
+//    NSString* hierarchyKey = [NSString stringWithFormat:@"%@.%@", nameSpace, key];
+//    
+//    NSMutableDictionary* query = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
+//    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecClass, kSecClassGenericPassword);
+//    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrService, (__bridge const void*)hierarchyKey);
+//    
+//    if (!obj) {
+//        return SecItemDelete((__bridge CFMutableDictionaryRef)query) == errSecSuccess;
+//    }
+//
+//    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecValueData, (__bridge const void*)obj);
+//    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrAccessible, accessibility);
+//
+//    OSStatus status = SecItemAdd((__bridge CFMutableDictionaryRef)query, NULL);
+//    if (status == errSecDuplicateItem) {
+//        NSMutableDictionary* update = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
+//        CFDictionarySetValue((__bridge CFMutableDictionaryRef)update, kSecValueData, CFDictionaryGetValue((__bridge CFMutableDictionaryRef)query, kSecValueData));
+//        CFDictionarySetValue((__bridge CFMutableDictionaryRef)update, kSecAttrAccessible, accessibility);
+//        CFDictionaryRemoveValue((__bridge CFMutableDictionaryRef)query, kSecAttrAccessible);
+//        return (SecItemUpdate((__bridge CFMutableDictionaryRef)query, (__bridge CFMutableDictionaryRef)update) == errSecSuccess);
+//    }
+//    return status == errSecSuccess;
+//}
+//
+//+ (NSData*) dataForKey:(NSString*)key {
+//    return [self dataForKey:key inNameSpace:[self bundleIdentifier]];
+//}
+//
+//+ (NSData*) dataForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    NSString* hierarchyKey = [NSString stringWithFormat:@"%@.%@", nameSpace, key];
+//    NSMutableDictionary* query = (__bridge_transfer NSMutableDictionary*)CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
+//    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecClass, kSecClassGenericPassword);
+//    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecAttrService, (__bridge const void*)hierarchyKey);
+//    CFDictionarySetValue((__bridge CFMutableDictionaryRef)query, kSecReturnData, kCFBooleanTrue);
+//    CFDataRef data;
+//    if (SecItemCopyMatching((__bridge CFMutableDictionaryRef)query, (CFTypeRef*)&data) != errSecSuccess) {
+//        return nil;
+//    }
+//    return (__bridge_transfer NSData*)data;
+//}
+//
+//+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key {
+//    return [self setJSONObject:object forKey:key inNameSpace:[self bundleIdentifier]];
+//}
+//
+//+ (BOOL) setJSONObject:(id)object forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    if (!object) {
+//        return [self setData:nil forKey:key inNameSpace:nameSpace];
+//    }
+//    if (![NSJSONSerialization isValidJSONObject:object]) {
+//        @throw NSInvalidArgumentException;
+//    }
+//    return [self setData:[NSJSONSerialization dataWithJSONObject:object options:0 error:nil] forKey:key inNameSpace:nameSpace];
+//}
+//
+//+ (id) objectForKey:(NSString*)key {
+//    return [self objectForKey:key inNameSpace:[self bundleIdentifier]];
+//}
+//
+//+ (id) objectForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    NSData* data = [self dataForKey:key inNameSpace:nameSpace];
+//    if (data) {
+//        return [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+//    }
+//    return nil;
+//}
+//
+//+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key {
+//    return [self setDate:date forKey:key inNameSpace:[self bundleIdentifier]];
+//}
+//
+//+ (BOOL) setDate:(NSDate*)date forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    NSDateFormatter* formatter = [NSDateFormatter new];
+//    formatter.dateFormat = _sIsoFormat;
+//    NSString* dateString = [formatter stringFromDate:date];
+//    return [self setString:dateString forKey:key inNameSpace:nameSpace];
+//}
+//
+//+ (NSDate*) dateForKey:(NSString*)key {
+//    return [self dateForKey:key inNameSpace:[self bundleIdentifier]];
+//}
+//
+//+ (NSDate*) dateForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    NSDateFormatter* formatter = [NSDateFormatter new];
+//    formatter.dateFormat = _sIsoFormat;
+//    return [formatter dateFromString:[self stringForKey:key inNameSpace:nameSpace]];
+//}
+//
+//+ (BOOL) setString:(NSString*)value forKey:(NSString*)key {
+//    return [self setString:value forKey:key inNameSpace:[self bundleIdentifier]];
+//}
+//
+//+ (BOOL) setString:(NSString*)value forKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    return [self setData:[value dataUsingEncoding:NSUTF8StringEncoding] forKey:key inNameSpace:nameSpace];
+//}
+//
+//+ (NSString*) stringForKey:(NSString*)key {
+//    return [self stringForKey:key inNameSpace:[self bundleIdentifier]];
+//}
+//     
+//+ (NSString*) stringForKey:(NSString*)key inNameSpace:(NSString*)nameSpace {
+//    NSData* data = [self dataForKey:key inNameSpace:nameSpace];
+//    return data ? [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] : nil;
+//}
 
 @end
