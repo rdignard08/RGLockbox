0059a053536e5be93bd0b853be9dde407636c879
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-05-31 13:18:01 -0700

    add doc comments to RGLockbox+Convenience

--- RGLockbox/RGLockbox+Convenience.swift
+++ RGLockbox/RGLockbox+Convenience.swift
@@ -23,6 +23,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 import Foundation
 
+/**
+ Provides a per-thread `NSDateFormatter` with `dateFormat` set to parse ISO style strings.
+*/
 func rg_stored_date_formatter() -> NSDateFormatter {
     let _sIsoFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
     let _sStoreFormatterKey = "rg_stored_date_formatter"
@@ -36,28 +39,51 @@ func rg_stored_date_formatter() -> NSDateFormatter {
     return formatter as! NSDateFormatter
 }
 
+/**
+ Provides additional functionality over the raw data based implementation.  Supports `NSDate`, `String`, `id<NSCoding>`
+*/
 extension RGLockbox {
     
+/**
+- parameter key: Identifer to search for in the manager's service name.
+- returns: A JSON equivalent object (`Array` or `Dictionary`) or `nil` (item not found).
+*/
     public func JSONObjectForKey(key:String) -> AnyObject? {
         let data = self.dataForKey(key)
         return data != nil ? try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions(rawValue: 0)) : nil
     }
     
+/**
+- parameter object: An `Array` or `Dictionary` object that is convertible by `NSJSONSerialization` or `nil`.  `nil` unsets the stored value.
+- parameter key: Location in the manager's service to store the resulting data.
+*/
     public func setJSONObject(object:AnyObject?, key:String) {
         let data = object != nil ? try! NSJSONSerialization.dataWithJSONObject(object!, options: NSJSONWritingOptions(rawValue: 0)) : nil as NSData?
         self.setData(data, forKey: key)
     }
     
+/**
+- parameter key: Identifer to search for in the manager's service name.
+- returns: A `String` object decoded from UTF-8 or `nil` (item not found).
+*/
     public func stringForKey(key:String) -> String? {
         let data = self.dataForKey(key)
         return data != nil ? String.init(data: data!, encoding: NSUTF8StringEncoding) : nil
     }
     
+/**
+- parameter string: A `String` object that is convertible by to UTF-8. or `nil`.  `nil` unsets the stored value.
+- parameter key: Location in the manager's service to store the resulting data.
+*/
     public func setString(string:String?, key:String) {
         let data = string?.dataUsingEncoding(NSUTF8StringEncoding)
         self.setData(data, forKey: key)
     }
     
+/**
+- parameter key: Identifer to search for in the manager's service name.
+- returns: A `NSDate` object decoded based on ISO format or `nil` (item not found or parse failure).
+*/
     public func dateForKey(key:String) -> NSDate? {
         let data = self.dataForKey(key)
         if (data != nil) {
@@ -67,18 +93,30 @@ extension RGLockbox {
         return nil
     }
     
+/**
+- parameter date: A `NSDate` object or `nil`. `nil` unsets the stored value.
+- parameter key: Location in the manager's service to store the resulting data.
+*/
     public func setDate(date:NSDate?, key:String) {
         let dateString = date != nil ? rg_stored_date_formatter().stringFromDate(date!) : nil as String?
         let data = dateString?.dataUsingEncoding(NSUTF8StringEncoding)
         self.setData(data, forKey: key)
     }
     
+/**
+- parameter key: Identifer to search for in the manager's service name.
+- returns: An object conforming to `NSCoding` created by `NSKeyedUnarchiver` or `nil` (item not found).
+*/
     public func codeableForKey(key:String) -> NSCoding? {
         let data = self.dataForKey(key)
         let ret = data != nil ? NSKeyedUnarchiver.unarchiveObjectWithData(data!) : nil as AnyObject?
         return ret as! NSCoding?
     }
     
+/**
+- parameter codeable: An object conforming to `NSCoding` or `nil`.  `nil` unsets the stored value.
+- parameter key: Location in the manager's service to store the resulting data.
+*/
     public func setCodeable(codeable:NSCoding?, key:String) {
         let data = codeable != nil ? NSKeyedArchiver.archivedDataWithRootObject(codeable!) : nil as NSData?
         self.setData(data, forKey: key)
